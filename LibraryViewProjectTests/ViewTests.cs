7de15f798998584403c14f15cb367d6c2c36869c
davidebbo <david.ebbo@microsoft.com>
2013-01-29 11:36:42 -0800

    Add simple unit test

---
+++ LibraryViewProjectTests/ViewTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RazorGenerator.Testing;
+
+namespace LibraryViewProjectTests
+{
+    [TestClass]
+    public class ViewTests
+    {
+        [TestMethod]
+        public void TestIndexView()
+        {
+            const string message = "Some unit test message!";
+
+            // Instantiate the view directly
+            var view = new LibraryViewProject.Views.Home.Index();
+
+            // Set up the data that needs to be access by the view
+            view.ViewBag.Message = message;
+
+            // Render it in an HtmlDocument
+            string output = view.Render();
+
+            // Verify that it looks correct
+            Assert.IsTrue(output.Contains(message));
+        }
+    }
+}
