c73186cf313aa6a54f8712eb73b976bd122bf176
davidebbo <david.ebbo@microsoft.com>
2013-11-20 18:23:12 -0800

    Add TestRenderAsHtml test

--- LibraryViewProjectTests/ViewTests.cs
+++ LibraryViewProjectTests/ViewTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using HtmlAgilityPack;
+using LibraryViewProject.Views.Home;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RazorGenerator.Testing;
 
@@ -13,16 +15,39 @@ namespace LibraryViewProjectTests
             const string message = "Some unit test message!";
 
             // Instantiate the view directly
-            var view = new LibraryViewProject.Views.Home.Index();
+            var view = new Index();
 
             // Set up the data that needs to be access by the view
             view.ViewBag.Message = message;
 
-            // Render it in an HtmlDocument
+            // Render it as a string
             string output = view.Render();
 
             // Verify that it looks correct
             Assert.IsTrue(output.Contains(message));
+        }
+
+        [TestClass]
+        public class UnitTest1
+        {
+            [TestMethod]
+            public void TestRenderAsHtml()
+            {
+                const string message = "Some unit test message!";
+
+                // Instantiate the view directly
+                var view = new Index();
+
+                // Set up the data that needs to be access by the view
+                view.ViewBag.Message = message;
+
+                // Render it in an HtmlDocument
+                var doc = view.RenderAsHtml();
+
+                // Verify that it looks correct
+                HtmlNode node = doc.DocumentNode.Element("h2");
+                Assert.AreEqual(message, node.InnerHtml.Trim());
+            }
         }
     }
 }
